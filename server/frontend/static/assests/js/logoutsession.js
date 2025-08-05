45a52b0df10b36bddb2cbc4dc06070ea259ace52
Rehman Malekear <rmalekar1992@gmail.com>
2025-08-05 00:00:43 -0400

    added logout functiionality

--- server/frontend/static/assests/js/logoutsession.js
+++ server/frontend/static/assests/js/logoutsession.js
@@ -1,4 +1,3 @@
-const logout = async (e) => {
 // Build logout URL and Make GET request to logout endpoint
   let logout_url = window.location.origin+"/djangoapp/logout";
   const res = await fetch(logout_url, {
@@ -18,18 +17,3 @@ const logout = async (e) => {
   else {
     alert("The user could not be logged out.")
   }
-};
-
-let checkSession = ()=>{
-  let curr_user = sessionStorage.getItem("username");
-
-  if (curr_user && curr_user !== "") {
-    document.getElementById("loginlogout").innerHTML = 
-    '<span class="homepage_links">' + curr_user +'</span>' +
-    '<a class="homepage_links" onclick="logout()" href="/">Logout</a>'
-  } else {
-    document.getElementById("loginlogout").innerHTML = 
-    '<a class="homepage_links" href="/login">Login</a>'+
-    '<a class="homepage_links"  href="/register">Register</a>'
-  }
- }
