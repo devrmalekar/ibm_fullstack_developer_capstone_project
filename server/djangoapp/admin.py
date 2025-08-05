6787fc5fd9eb5aff36c5889214e8c6e874daa0f3
Rehman Malekar <rmalekar1992@gmail.com>
2025-08-05 05:17:22 -0400

    registered models on admin site and instantiated method to generate dummy car make and model data

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -2,13 +2,25 @@ from django.contrib import admin
 from .models import CarMake, CarModel
 
 
-# Register your models here.
-admin.site.register(CarMake)
-admin.site.register(CarModel)
+
 # CarModelInline class
+class CarModelInline(admin.StackedInline):
+    model = CarModel    
+    extra = 1
 
 # CarModelAdmin class
+class CarModelAdmin(admin.ModelAdmin):
+    fields = ['car_make', 'name', 'car_type', 'year']
 
 # CarMakeAdmin class with CarModelInline
-
+class CarMakeAdmin(admin.ModelAdmin):
+    fields = ['name', 'description']
+    inlines = [CarModelInline]
 # Register models here
+
+# Register your models here.
+admin.site.register(CarMake, CarMakeAdmin)
+admin.site.register(CarModel, CarModelAdmin)
+
+
+
