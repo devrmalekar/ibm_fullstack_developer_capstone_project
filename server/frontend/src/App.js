f7d283cc8aaa7fdccec5023983bc89fea973c9b9
Rehman Malekear <rmalekar1992@gmail.com>
2025-08-05 00:16:25 -0400

    Fixed issue with registration functionality

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/login" element={<LoginPanel />} />
-      <Route path="/login" element={<RegisterPanel />} />
+      <Route path="/register" element={<RegisterPanel />} />
     </Routes>
   );
 }
