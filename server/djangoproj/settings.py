c3ab0721b0e0d42734a939bcdf79c3737fb86d11
rmalekar <rmalekar1992@gmail.com>
2025-08-06 13:01:51 +1000

    Added migration for djangoapp and added allowed host to localhost in settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -30,10 +30,12 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     "localhost",
+    "127.0.0.1",
     "rmalekar1992-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy"
     ".cognitiveclass.ai",
 ]
 CSRF_TRUSTED_ORIGINS = [
+    "http://127.0.01",
     "https://rmalekar1992-8000.theiadockernext-1-labs-prod-theiak8s"
     "-4-tor01.proxy.cognitiveclass.ai"
 ]
